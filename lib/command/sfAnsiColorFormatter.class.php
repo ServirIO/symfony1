b2a9a38cc4f128a8cadd7553f5652e08742180d0
ddeleon-servir <ddeleon@servirsa.com>
2025-07-12 10:51:11 -0600

    fix(sfAnsiColorFormatter) Unsopported operand types

--- lib/command/sfAnsiColorFormatter.class.php
+++ lib/command/sfAnsiColorFormatter.class.php
@@ -86,10 +86,16 @@ class sfAnsiColorFormatter extends sfFormatter
             $size = $this->size;
         }
 
+        $size = (int) $size; // Ensure $size is always an integer
+
         $style = array_key_exists($style, $this->styles) ? $style : 'INFO';
         $width = 9 + strlen($this->format('', $style));
 
-        return sprintf(">> %-{$width}s %s", $this->format($section, $style), $this->excerpt($text, $size - 4 - (strlen($section) > 9 ? strlen($section) : 9)));
+        return sprintf(
+            ">> %-{$width}s %s",
+            $this->format($section, $style),
+            $this->excerpt($text, $size - 4 - (strlen($section) > 9 ? strlen($section) : 9))
+        );
     }
 
     /**
